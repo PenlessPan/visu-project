a064321c906ff7d8ba397923c42760db079b7e02
PenlessPan <106378478+PenlessPan@users.noreply.github.com>
2023-06-23 19:43:34 +0300

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -2,6 +2,104 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import os
+from matplotlib.patches import Circle, RegularPolygon
+from matplotlib.path import Path
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
+
+
+def radar_factory(num_vars, frame='polygon'):
+    """
+    Create a radar chart with `num_vars` axes.
+
+    This function creates a RadarAxes projection and registers it.
+
+    Parameters
+    ----------
+    num_vars : int
+        Number of variables for radar chart.
+    frame : {'circle', 'polygon'}
+        Shape of frame surrounding axes.
+
+    """
+    # calculate evenly-spaced axis angles
+    theta = np.linspace(0, 2*np.pi, num_vars, endpoint=False)
+
+    class RadarTransform(PolarAxes.PolarTransform):
+
+        def transform_path_non_affine(self, path):
+            # Paths with non-unit interpolation steps correspond to gridlines,
+            # in which case we force interpolation (to defeat PolarTransform's
+            # autoconversion to circular arcs).
+            if path._interpolation_steps > 1:
+                path = path.interpolated(num_vars)
+            return Path(self.transform(path.vertices), path.codes)
+
+    class RadarAxes(PolarAxes):
+
+        name = 'radar'
+        PolarTransform = RadarTransform
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            # rotate plot such that the first axis is at the top
+            self.set_theta_zero_location('N')
+
+        def fill(self, *args, closed=True, **kwargs):
+            """Override fill so that line is closed by default"""
+            return super().fill(closed=closed, *args, **kwargs)
+
+        def plot(self, *args, **kwargs):
+            """Override plot so that line is closed by default"""
+            lines = super().plot(*args, **kwargs)
+            for line in lines:
+                self._close_line(line)
+
+        def _close_line(self, line):
+            x, y = line.get_data()
+            # FIXME: markers at x[0], y[0] get doubled-up
+            if x[0] != x[-1]:
+                x = np.append(x, x[0])
+                y = np.append(y, y[0])
+                line.set_data(x, y)
+
+        def set_varlabels(self, labels):
+            self.set_thetagrids(np.degrees(theta), labels)
+
+        def _gen_axes_patch(self):
+            # The Axes patch must be centered at (0.5, 0.5) and of radius 0.5
+            # in axes coordinates.
+            if frame == 'circle':
+                return Circle((0.5, 0.5), 0.5)
+            elif frame == 'polygon':
+                return RegularPolygon((0.5, 0.5), num_vars,
+                                      radius=.5, edgecolor="k")
+            else:
+                raise ValueError("Unknown value for 'frame': %s" % frame)
+
+        def _gen_axes_spines(self):
+            if frame == 'circle':
+                return super()._gen_axes_spines()
+            elif frame == 'polygon':
+                # spine_type must be 'left'/'right'/'top'/'bottom'/'circle'.
+                spine = Spine(axes=self,
+                              spine_type='circle',
+                              path=Path.unit_regular_polygon(num_vars))
+                # unit_regular_polygon gives a polygon of radius 1 centered at
+                # (0, 0) but we want a polygon of radius 0.5 centered at (0.5,
+                # 0.5) in axes coordinates.
+                spine.set_transform(Affine2D().scale(.5).translate(.5, .5)
+                                    + self.transAxes)
+                return {'polar': spine}
+            else:
+                raise ValueError("Unknown value for 'frame': %s" % frame)
+
+    register_projection(RadarAxes)
+    return theta
+
+
 df = pd.read_csv('./dog_breeds.csv')
 df = df.drop(df[df['drooling'] == 0].index, inplace=False)
 attributes = pd.DataFrame()
@@ -89,19 +187,14 @@ else:
 selected_breed_attributes = get_breed_attributes(selected_breed)
 
 # Spider plot
-fig, ax = plt.subplots()
-selected_breed_attributes = selected_breed_attributes.append(selected_breed_attributes[:1])  # Complete the loop
-ax.plot(selected_breed_attributes.values, marker='o')
-ax.fill(selected_breed_attributes.values, alpha=0.3)
-ax.set_xticks(range(len(attribute_names)))
-ax.set_xticklabels(attribute_names)
+theta = radar_factory(6)
+data = list(attributes.loc[attributes['Name'] == "Beagle"].drop(['Name'], axis=1).iloc[0])
+vals = attributes.drop(['Name'], axis=1)
+attribute_names = list(vals.columns)
+fig, axs = plt.subplots(figsize=(3, 3), nrows=1, ncols=1,
+                        subplot_kw=dict(projection='radar'))
+axs.plot(theta, data)
+axs.fill(theta, data, alpha=0.45)
+axs.set_varlabels(attribute_names)
 st.pyplot(fig)
-
-
-
-
-
-
-
-
 
