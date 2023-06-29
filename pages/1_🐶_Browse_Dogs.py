8ae254692ff8166dcaced836cc1633a4ace3a8bb
PenlessPan <106378478+PenlessPan@users.noreply.github.com>
2023-06-29 15:59:28 +0300

    nigga

--- pages/1_🐶_Browse_Dogs.py
+++ pages/1_🐶_Browse_Dogs.py
@@ -10,8 +10,7 @@ from matplotlib.projections import register_projection
 import numpy as np
 
 st.set_page_config(page_title="Meet Our Dogs!", layout="wide")
-df = pd.read_csv(
-    r"dog_attributes.csv")
+df = pd.read_csv(r"dog_attributes.csv")
 
 # Function to create spider plots
 def radar_factory(num_vars, frame='polygon'):
@@ -145,7 +144,7 @@ def display_dog_table(df):
             breed_row = df[row * num_columns: (row + 1) * num_columns]["Name"]
             breed_columns = st.columns(num_columns)
             for breed, column in zip(breed_row, breed_columns):
-                image_path = fr".\dog_pics\{breed}.png"
+                image_path = fr"dog_pics\{breed}.png"
                 column.image(image_path, width=100)
                 column.button(breed, on_click=show_stats, args=(breed,))
     breed_details_container = st.container()
